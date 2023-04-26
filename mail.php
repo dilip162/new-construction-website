a51c11395c3d4fe175418ce52dbe8cdb6465f949
dilip baghel <dilip.18baghel@gmail.com>
2023-04-26 13:31:31 +0530

    adding smtp sevice and thankyou page

--- mail.php
+++ mail.php
@@ -1,66 +1,19 @@
 <?php
-$message = ""; 
-if(isset($_POST['submit'])){
-	$to = "dilipbaghel162@gmail.com"; // Your email address
-	$name = $_POST['name'];
-	$from = $_POST['email'];
-	$subject = $_POST['subject'];
-	$company = $_POST['company'];
-	$enquiry = $_POST['enquiry'];
-	$message = "<!DOCTYPE html>
-	<html>
-	<head>
-	</head>
-	<body>
-	<table rules='all' border='1' style='border-color: #666;' cellpadding='10'>
-    <tr style='background: #eee;'><td colspan='2'><strong>Contact Form Details</strong> </td></tr>
-    <tr>
-        <td><strong>Name:</strong></td>
-        <td>".$_POST['name']."</td>
-    </tr>
-    <tr>
-        <td><strong>Mobile:</strong></td>
-        <td>".$_POST['subject']."</td>
-    </tr>
-    <tr>
-        <td><strong>Email:</strong></td>
-        <td>".$_POST['email']."</td>
-    </tr>
-    <tr>
-        <td><strong>Email:</strong></td>
-        <td>".$_POST['company']."</td>
-    </tr>
-    <tr>
-        <td><strong>Email:</strong></td>
-        <td>".$_POST['enquiry']."</td>
-    </tr>
-    <tr>
-        <td><strong>Message:</strong></td>
-        <td>".$_POST['message']."</td>
-    </tr>
-	</table>
-	</body>
-	</html>";
-	
-	$subject = "Contact Form Details";
-	
-	// Set content-type header for sending HTML email 
-	$headers = "MIME-Version: 1.0" . "\r\n"; 
-	$headers .= "Content-type:text/html;charset=UTF-8" . "\r\n";
-	
-	$headers .= "From:" . $from . "\r\n";
-	
-	$result = mail($to,$subject,$message,$headers);
-	if ($result) {
-		// $message = "Your Message was sent Successfully!";
-		echo '<script type="text/javascript">alert("Your Message was sent Successfully!");</script>';
-		echo '<script type="text/javascript">window.location.href = window.location.href;</script>';
+//get data from form  
 
-	}else{
-		// $message = "Sorry! Message was not sent, Try again Later.";
-		echo '<script type="text/javascript">alert("Sorry! Message was not sent, Try again Later.");</script>';
-		echo '<script type="text/javascript">window.location.href = window.location.href;</script>';
-	}
-	// header('Location: contact.php');
+$name = $_POST['name'];
+$email= $_POST['email'];
+$subject= $_POST['subject'];
+$company= $_POST['company'];
+$enquiry= $_POST['enquiry'];
+$to = "dilipbaghel162@gmail.com";
+$subject = "Mail From website";
+$txt ="Name = ". $name . "\r\n  Email = " . $email . "\r\n subject =" . $subject "\r\n company =" . $company "\r\n enquiry =" . $enquiry;
+$headers = "From: noreply@yoursite.com" . "\r\n" .
+"CC: somebodyelse@example.com";
+if($email!=NULL){
+    mail($to,$subject,$txt,$headers);
 }
+//redirect
+header("Location:thankyou.html");
 ?>
